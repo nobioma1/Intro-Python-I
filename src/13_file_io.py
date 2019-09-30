3dae0e26a84fd6ffdc4a51aef03f4746e5b9e0a1
Noble Obioma <nobioma1@gmail.com>
2019-09-30 15:34:07 +0100

    Read and Write using python file IO

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+foo_file = open('foo.txt', 'r')
+foo_file_contents = foo_file.read()
+print(foo_file_contents)
+foo_file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar_file = open('bar.txt', 'w+')
+bar_file.write('Writing my first lines in Python IO\n')
+bar_file.write('Hello, Reader\n')
+bar_file.write('Bye for now\n')
+bar_file.close()
