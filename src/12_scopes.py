686ff973e8bdf97dee348b0f33b3f19060be4a03
Noble Obioma <nobioma1@gmail.com>
2019-09-30 15:21:43 +0100

    fix scope global, local and function scope(global, nonlocal)

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,12 +5,13 @@
 x = 12
 
 def changeX():
+    global x # get to view x in function scope globally
     x = 99
 
 changeX()
 
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
-print(x)
+print(x) # adding 'global x' to the changeX function
 
 
 # This nested function has a similar problem.
@@ -19,12 +20,13 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y # get to view y in function scope in outer function scope
         y = 999
 
     inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
-    print(y)
+    print(y) # adding 'non local y' to the inner function
 
 outer()
